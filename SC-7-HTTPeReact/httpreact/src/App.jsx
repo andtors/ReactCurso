406acc021bab3dff8735e7dca32980892003a418
André Torres <andre.tors448@gmail.com>
2024-04-11 15:50:37 -0300

    HTTP com React

---
+++ SC-7-HTTPeReact/httpreact/src/App.jsx
@@ -0,0 +1,38 @@
+import './App.css'
+
+import { useState, useEffect } from 'react'
+
+function App() {
+const [products, setProducts] = useState([])
+
+  const url = "http://localhost:3000/products"
+  
+
+  // 1 - Resgatando dados
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(url)
+      const data = await res.json()
+  
+      setProducts(data)
+    }
+    fetchData()
+  }, [])
+
+
+
+  return (
+    <>
+      <div className="App">
+        <h1>Lista de produtos:</h1>
+        <ul>
+          {products.map((p)  => (
+            <li key={p.id}>{p.name} - R$ {p.price}</li>
+          ))}
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default App
